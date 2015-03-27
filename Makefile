c51d10db177361b5ce0a8c0abcae27683e0110f0
Nicolas Lempereur <nle@odoo.com>
2015-03-27 21:36:08 +0100

    first version

---
+++ Makefile
@@ -0,0 +1,14 @@
+.PHONY: update
+
+extension.zip:	extension/e.js extension/manifest.json
+	rm -f $@
+	7z -mx=9 a $@ extension
+
+extension/e.js: script.js
+	closure --compilation_level ADVANCED_OPTIMIZATIONS --js script.js > $@
+
+extension/manifest.json:	manifest.json
+	cat manifest.json | tr '\n' ' ' |sed -r 's/([\{\},:\["]|\])\s+/\1/g' > $@
+
+clean:
+	rm -f extension{.zip,/{e.js,manifest.json}}
---
+++ script.js
@@ -0,0 +1,29 @@
+function insertAfter(new_node, ref_node) {
+  ref_node.parentNode.insertBefore(new_node, ref_node.nextSibling);
+}
+function treat(node) {
+  switch(node.nodeType) {
+    case 3:
+      break;
+    case 1:
+      [].slice.call(node.childNodes).forEach(treat);
+    default:
+      return;
+  }
+  var oval = node.nodeValue;
+  if (oval.indexOf("opw") == -1) {
+    return;
+  }
+  var done_offset = 0;
+  oval.replace(/\b(opw[: -] ?)(\d{6,})\b/g, function(match, prefix, num, offset) {
+    var link = document.createElement("a");
+    link.setAttribute("href", "https://www.odoo.com/web#id=" + num + "&view_type=form&model=project.issue");
+    link.appendChild(document.createTextNode(num));
+    node.nodeValue = oval.slice(done_offset, offset + prefix.length);
+    done_offset = offset + match.length;
+    insertAfter(link, node);
+    node = document.createTextNode(oval.slice(done_offset));
+    insertAfter(node, link);
+  });
+}
+[].slice.call(document.querySelectorAll("div.comment-body,div.commit-desc")).forEach(treat);
