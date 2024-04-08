bce3262e239b9df54f994a91c8b7aa7d0ba2f523
Achraf (abz) <abz@odoo.com>
2024-04-08 06:57:24 +0200

    clean code + fix duplicate copy button

--- e.js
+++ e.js
@@ -7,56 +7,73 @@
 const insertAfter = (new_node, ref_node) => {
   ref_node.parentNode.insertBefore(new_node, ref_node.nextSibling);
 }
-const treat = node => {
-  switch(node.nodeType) {
-    case 3:
-      break;
-    case 1:
-      node.childNodes.forEach(treat);
-    default:
-      return;
-  }
-  const oval = node.nodeValue;
-  if (!/opw|task/i.test(oval)) {
-    return;
-  }
-  let done_offset = 0;
-  oval.replace(/\b((?:opw|task)(?: id)?[: #-] ?)(\d{5,})\b/gi, function(match, prefix, num, offset) {
-    const link = document.createElement("a");
-    num = prefix[0].toLowerCase() == 'o' && num < 1E6 ? 1E6 + +num : num;
-    link.setAttribute("href", "https://www.odoo.com/web#id=" + num + "&view_type=form&model=project.task");
-    link.appendChild(document.createTextNode(num));
-    node.nodeValue = oval.slice(done_offset, offset + prefix.length);
-    done_offset = offset + match.length;
-    insertAfter(link, node);
-    node = document.createTextNode(oval.slice(done_offset));
-    insertAfter(node, link);
-  });
-}
-const treat_all = () => {
-  document.querySelectorAll(".comment-body:not([data-opw-handled]),.commit-desc:not([data-opw-handled])").forEach(n => {
-    n.setAttribute('data-opw-handled', 1);
-    treat(n)
-  })
-  //add copy to clipboard button without remote:
+
+
+const addOPWCopyButton = () => {
+  if (document.querySelector('.opw-copy')) return;
+
   document.querySelectorAll("span clipboard-copy").forEach(el => {
     const el_copy = el.cloneNode(true);
     const svg = el_copy.querySelector('svg');
     const value = el_copy.getAttribute('value');
 
-    if (!~value.indexOf('odoo-dev:')) return;
+    if (!~value.indexOf(':')) return;
 
     svg.style.color = 'green';
     el_copy.setAttribute('value', value.substr(value.indexOf(':') + 1));
-
+    el_copy.classList.add('opw-copy');
     el.parentElement.appendChild(el_copy);
   });
+}
 
+const addOPWLinks = () => {
+  const treat = node => {
+    switch(node.nodeType) {
+      case 3:
+        break;
+      case 1:
+        node.childNodes.forEach(treat);
+      default:
+        return;
+    }
+    const oval = node.nodeValue;
+    if (!/opw|task/i.test(oval)) {
+      return;
+    }
+    let done_offset = 0;
+    oval.replace(/\b((?:opw|task)(?: id)?[: #-] ?)(\d{5,})\b/gi, function(match, prefix, num, offset) {
+      const link = document.createElement("a");
+      num = prefix[0].toLowerCase() == 'o' && num < 1E6 ? 1E6 + +num : num;
+      link.setAttribute("href", "https://www.odoo.com/web#id=" + num + "&view_type=form&model=project.task");
+      link.appendChild(document.createTextNode(num));
+      node.nodeValue = oval.slice(done_offset, offset + prefix.length);
+      done_offset = offset + match.length;
+      insertAfter(link, node);
+      node = document.createTextNode(oval.slice(done_offset));
+      insertAfter(node, link);
+    });
+  }
+
+  document.querySelectorAll(".comment-body:not([data-opw-handled]),.commit-desc:not([data-opw-handled])").forEach(n => {
+    n.setAttribute('data-opw-handled', 1);
+    treat(n)
+  })
+}
+
+const addCommitDirectClick = () => {
   if (document.querySelector('a.tabnav-tab.selected[href$="/commits"] #commits_tab_counter[title="1"]')) {
     let commits = document.querySelectorAll('.TimelineItem-body a.Link--primary');
     if (commits.length === 1) {
         commits[0].click();
     }
   }
-};
-window.addEventListener('turbo:load', treat_all);
+}
+
+window.addEventListener('turbo:load', () => {
+  //replace opw-1234 by a link to the odoo.comtask:
+  addOPWLinks();
+  //add copy to clipboard button without remote:
+  addOPWCopyButton();
+  //direct click on commit tab if only one commit
+  addCommitDirectClick();
+});
